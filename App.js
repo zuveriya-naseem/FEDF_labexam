2742dc59ff4a2aa29278bb8b2f00f7306bb21e33
zuveriya-naseem <2400080054@kluniversity.in>
2025-10-17 10:52:32 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,18 @@
 function greet() {
-  console.log("Hello World");
+  const greetings = [
+    "Good Morning",
+    "Good Afternoon",
+    "Good Evening",
+    "Have a great day ahead!",
+  ];
+
+  console.log("----- Greetings from the App -----");
+  greetings.forEach((msg, index) => {
+    console.log(`${index + 1}. ${msg}`);
+  });
+  console.log("----------------------------------");
 }
+
+greet();
+
+
